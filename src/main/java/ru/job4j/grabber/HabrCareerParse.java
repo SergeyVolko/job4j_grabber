5779243ab50c9082cbf9e9a99fc6b44b5d04e55d
Lenovo <rusobraz@mail.ru>
2022-05-16 18:20:11 +0300

    2.3. Загрузка деталей поста. [#285212]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -1,6 +1,5 @@
 package ru.job4j.grabber;
 
-import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -15,10 +14,23 @@ public class HabrCareerParse {
     private static final String PAGE_LINK =
             String.format("%s/vacancies/java_developer?page=", SOURCE_LINK);
 
-    public static void main(String[] args) throws IOException {
+    private static String retrieveDescription(String link) {
+        String result = "";
+        try {
+            result = getDocument(link).select(".style-ugc").text();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    private static Document getDocument(String link) throws IOException {
+        return Jsoup.connect(link).get();
+    }
+
+    private static void showVacancies() throws IOException {
         for (int i = 1; i <= 5; i++) {
-            Connection connection = Jsoup.connect(PAGE_LINK + i);
-            Document document = connection.get();
+            Document document = getDocument(PAGE_LINK + i);
             Elements rows = document.select(".vacancy-card__inner");
             rows.forEach(row -> {
                 Element titleElement = row.select(".vacancy-card__title").first();
@@ -27,9 +39,14 @@ public class HabrCareerParse {
                 String vacancyName = titleElement.text();
                 String vacancyDate = dateElement.attr("datetime");
                 String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
-                System.out.printf("%s %s %s%n", vacancyName, link, vacancyDate);
+                System.out.println("-".repeat(100));
+                System.out.printf("%s %s %s %n%n %s%n",
+                        vacancyName, link, vacancyDate, retrieveDescription(link));
             });
         }
+    }
 
+    public static void main(String[] args) throws IOException {
+       showVacancies();
     }
 }
