837166a565626070bdace62730e1742ec86889be
Lenovo <rusobraz@mail.ru>
2022-05-16 17:57:15 +0300

    2.2. Модель данных - Post. [#285211](исправлено)

--- src/main/java/ru/job4j/model/Post.java
+++ src/main/java/ru/job4j/model/Post.java
@@ -10,18 +10,21 @@ public class Post {
     private int id;
     private String title;
     private String link;
+    private String description;
     private LocalDateTime created;
 
-    public Post(int id, String title, String link, LocalDateTime created) {
+    public Post(int id, String title, String link, String description, LocalDateTime created) {
         this.id = id;
         this.title = title;
         this.link = link;
+        this.description = description;
         this.created = created;
     }
 
-    public Post(String title, String link, LocalDateTime created) {
+    public Post(String title, String link, String description, LocalDateTime created) {
         this.title = title;
         this.link = link;
+        this.description = description;
         this.created = created;
     }
 
@@ -35,6 +38,10 @@ public class Post {
 
     public String getLink() {
         return link;
+    }
+
+    public String getDescription() {
+        return description;
     }
 
     public LocalDateTime getCreated() {
@@ -51,6 +58,10 @@ public class Post {
 
     public void setLink(String link) {
         this.link = link;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public void setCreated(LocalDateTime created) {
